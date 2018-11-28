beda5580826f6a72036322bf54a1fba83ca5ebcd
Anam Ansari <ansarianam02@users.noreply.github.com>
2018-11-28 11:32:14 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,8 +6,93 @@
  var registMandatory = "Please fill All Mandatory fields";
  var ValidEmailError = "Please Enter Valid Email ID";
  var apiurl = "https://maazaprod.bigcityexperiences.com/v1/api/";
+ var tokenVal = '123123123';
+ function hideError(){
+    $('.err-container').hide();
+   }
+
+   var validateotp =function(otpNumber){
+
+var form = new FormData();
+form.append("token", tokenVal);
+form.append("otp", otpNumber);
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": apiurl+"validateotp",
+  "method": "POST",
+  "headers": {
+    "cache-control": "no-cache",
+    "postman-token": "33f15ab9-bef1-1aeb-da10-9a973d8fd501"
+  },
+  "processData": false,
+  "contentType": false,
+  "mimeType": "multipart/form-data",
+  "data": form
+}
+
+  
+$.ajax(settings).done(function (response) {
+  
+  var res =JSON.parse(response)
+  if(res.status==1){
+    // show More detail section
+    $('.otpSection').hide();
+    $('.moreDetailSection').show();
+  }
+ });
+
+}
+
+var updateuser =function(name,email,city,state){
+
+var tokenVal = '';
+var mobilenumVal = '';
+
+var form = new FormData();
+form.append("token", tokenVal);
+form.append("name", name);
+form.append("email", email);
+form.append("city", city);
+form.append("state", state);
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": apiurl+"updateuser",
+  "method": "POST",
+  "headers": {
+    "cache-control": "no-cache",
+    "postman-token": "29bb3f58-fcb2-c17d-5114-48b4c5035200"
+  },
+  "processData": false,
+  "contentType": false,
+  "mimeType": "multipart/form-data",
+  "data": form
+}
+
+
+//remove it later
+window.location='intro.html';
+$.ajax(settings).done(function (response) {
+   var res =JSON.parse(response)
+  if(res.status==1){
+    window.location='intro.html';   
+  }
+
+});
+}
 
   $(document).on('click','.btn-mobile-submit',function(e){
+    function showError(errMsg) {
+      $('.err-container').show();
+      $('.err-msg').html(errMsg);
+   }
+
+   function hideError(){
+    $('.err-container').hide();
+   }
   	hideError();
     //e.preventDefault();
     var phoneVal = $('#phonenumber').val();
@@ -17,6 +102,15 @@
     	showError(emptyMobileNumber);
     	return true;
     }
+
+    var phonenumber = function (inputtxt){
+  var phoneno = /^\d{10}$/;
+  if(inputtxt.match(phoneno)){
+          return true;
+    }else{
+        return false;
+   }
+ }
 
     var valid = phonenumber(phoneVal); 
     if(!valid){
@@ -29,6 +123,55 @@
       showError(termsError);
       return false;
 	}else{
+
+    var registeruser =function(mobilenum){
+
+var form = new FormData();
+form.append("token", tokenVal);
+form.append("mobilenum",mobilenum);
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": apiurl+"registeruser",
+  "method": "POST",
+  "headers": {
+    "cache-control": "no-cache",
+    "postman-token": "0307db79-5eec-1590-dade-133827dbc697"
+  },
+  "processData": false,
+  "contentType": false,
+  "mimeType": "multipart/form-data",
+  "data": form
+}
+
+$.ajax(settings).done(function (response) {
+  var res =JSON.parse(response)
+    if(res.status==1){
+       //show otp and hide Error
+     $('.regSection').hide();
+     $('.banner-container').remove();
+     $('.otpSection').show();
+     $('.err-container').hide();
+
+     var timeleft = 30;
+    var downloadTimer = setInterval(function(){
+    timeleft--;
+    document.getElementById("countdowntimer").textContent = timeleft;
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        document.getElementById("resendotp").classList.add("red-t");
+      }
+    },1000);
+
+    }else{
+
+    }
+    
+});
+
+}
+
 	   registeruser(phoneVal);    	
 	}	
     
@@ -236,39 +379,7 @@ $.ajax(settings).done(function (response) {
 'type' => 'SUCCESS', 'msg' => 'OTP OK', 'status' => 1, ]; $result = [
 'type' => 'ERROR', 'msg' => 'OTP not valid’, 'status' => 0, ];
 */
-var validateotp =function(otpNumber){
 
-var form = new FormData();
-form.append("token", tokenVal);
-form.append("otp", otpNumber);
-
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": apiurl+"validateotp",
-  "method": "POST",
-  "headers": {
-    "cache-control": "no-cache",
-    "postman-token": "33f15ab9-bef1-1aeb-da10-9a973d8fd501"
-  },
-  "processData": false,
-  "contentType": false,
-  "mimeType": "multipart/form-data",
-  "data": form
-}
-
-  
-$.ajax(settings).done(function (response) {
-  
-  var res =JSON.parse(response)
-  if(res.status==1){
-    // show More detail section
-    $('.otpSection').hide();
-    $('.moreDetailSection').show();
-  }
- });
-
-}
 
 
 /*
@@ -303,42 +414,7 @@ $.ajax({
 'type' => 'SUCCESS', 'msg' => 'Updated OK', 'status' => 1, ]; $result = [
 'type' => 'ERROR', 'msg' => 'Invalid ...’, 'status' => 0, ];
 */
-var updateuser =function(name,email,city,state){
 
-var tokenVal = '';
-var mobilenumVal = '';
-
-var form = new FormData();
-form.append("token", tokenVal);
-form.append("name", name);
-form.append("email", email);
-form.append("city", city);
-form.append("state", state);
-
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": apiurl+"updateuser",
-  "method": "POST",
-  "headers": {
-    "cache-control": "no-cache",
-    "postman-token": "29bb3f58-fcb2-c17d-5114-48b4c5035200"
-  },
-  "processData": false,
-  "contentType": false,
-  "mimeType": "multipart/form-data",
-  "data": form
-}
-//remove it later
-window.location='intro.html';
-$.ajax(settings).done(function (response) {
-   var res =JSON.parse(response)
-  if(res.status==1){
-  	window.location='intro.html';  	
-  }
-
-});
-}
 
 
 
